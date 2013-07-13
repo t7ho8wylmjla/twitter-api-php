8d43fe9553272a68108d000061a8f4377ee997dd
t7ho8wylmjla <t7ho8wylmjla@outlook.com>
2013-07-13 08:03:46 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
-    'oauth_access_token' => "",
+    'oauth_access_token' => "لاتلات",
     'oauth_access_token_secret' => "",
     'consumer_key' => "",
     'consumer_secret' => ""
